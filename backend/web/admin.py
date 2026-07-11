db58e9191ddf5fe37e9aaabb635103cc4107d175
trspnt <trspnt@trspnt.com>
2026-07-11 15:07:22 +0800

    定义了用户数据库并同步到管理员页面

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from web.models.user import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)  #逗号不要删,加了逗号表示传入一个元组

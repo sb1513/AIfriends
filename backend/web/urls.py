2bd5ac2fae36bdb9cdbee8d69e3c919aef7c362e
trspnt <trspnt@trspnt.com>
2026-07-17 23:50:39 +0800

    实现首页

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -9,6 +9,7 @@ from web.views.create.character.get_list import GetListCharacterView
 from web.views.create.character.get_single import GetSingleCharacterView
 from web.views.create.character.remove import RemoveCharacterView
 from web.views.create.character.update import UpdateCharacterView
+from web.views.homepage.index import HomepageIndexView
 from web.views.index import index
 from web.views.user.accounts import get_user_info
 from web.views.user.accounts.get_user_info import GetUserInfoView
@@ -30,6 +31,7 @@ urlpatterns = [
     path("api/create/character/remove/",RemoveCharacterView.as_view()),
     path("api/create/character/get_single/",GetSingleCharacterView.as_view()),
     path("api/create/character/get_list/",GetListCharacterView.as_view()),
+    path('api/homepage/index/',HomepageIndexView.as_view()),
     path('', index),
 
     re_path(r'^(?!media/|static/|assets/).*$', index),

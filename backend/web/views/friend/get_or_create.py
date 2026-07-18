4831b4fcf3a4473f5df0d796470312f6dd05c093
trspnt <trspnt@trspnt.com>
2026-07-18 22:09:09 +0800

    实现聊天框

--- backend/web/views/friend/get_or_create.py
+++ backend/web/views/friend/get_or_create.py
@@ -17,7 +17,7 @@ class GetOrCreateFriendView(APIView):
             if friends.exists():
                 friend = friends.first()
             else:
-                friend = Friend.objects.create(character_id=character_id,me=user)
+                friend = Friend.objects.create(character_id=character_id,me=user_profile)
             character = friend.character
             author = character.author
             return Response({

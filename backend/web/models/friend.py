c7ceaa0540cc4a812276df8d014260667bc9f3b3
Trspnt <None>
2026-07-29 14:26:09 +0800

    实现聊天记录，及流式加载

--- backend/web/models/friend.py
+++ backend/web/models/friend.py
@@ -9,8 +9,8 @@ class Friend(models.Model):
     me = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
     memory = models.TextField(default="",max_length=5000,blank=True,null=True)
-    create_time = models.DateTimeField(default=now())
-    update_time = models.DateTimeField(default=now())
+    create_time = models.DateTimeField(default=now)
+    update_time = models.DateTimeField(default=now)
 
     def __str__(self):
         return f"{self.character.name} - {self.me.user.username} - {localtime(self.create_time).strftime('%Y-%m-%d %H:%M:%S')}"
@@ -19,7 +19,7 @@ class Friend(models.Model):
 class Message(models.Model):
    friend = models.ForeignKey(Friend, on_delete=models.CASCADE)
    user_message = models.TextField(max_length=500)
-   input = models.TextField(max_length=500)
+   input = models.TextField(max_length=10000)
    output = models.TextField(max_length=500)
    input_tokens = models.IntegerField(default=0)
    output_tokens = models.IntegerField(default=0)
--- frontend/src/components/character/chat_field/input_field/InputField.vue
+++ frontend/src/components/character/chat_field/input_field/InputField.vue
@@ -6,6 +6,7 @@ import {ref, useTemplateRef} from "vue";
 import streamApi from "@/js/http/streamApi.js";
 
 const props = defineProps(['friendId'])
+const emit = defineEmits(['pushBackMessage', 'addToLastMessage'])
 const inputRef = useTemplateRef('input-ref')
 const message = ref('')
 let isProcessing = false
@@ -22,6 +23,9 @@ async function handleSend(){
   if(!content) return
   message.value = ''
 
+  emit('pushBackMessage', {role: 'user',content: content, id: crypto.randomUUID()})
+  emit('pushBackMessage', {role: 'ai', content: '', id: crypto.randomUUID()})
+
   try{
     await streamApi('/api/friend/message/chat/', {
       body: {
@@ -32,7 +36,7 @@ async function handleSend(){
         if (isDone) {
           isProcessing = false
         } else if(data.content){
-          console.log(data.content)
+          emit('addToLastMessage', data.content)
         }
       },
       onerror(err) {

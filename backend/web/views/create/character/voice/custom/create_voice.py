842d791978c7d02f698efae91798710851b6299b
Trspnt <None>
2026-08-01 19:30:05 +0800

    实现声音复刻

---
+++ backend/web/views/create/character/voice/custom/create_voice.py
@@ -0,0 +1,22 @@
+import os
+
+import requests
+
+
+def create_voice(voice_url, prefix):
+    headers = {
+        "Authorization": f"Bearer {os.getenv('API_KEY')}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model": "voice-enrollment",
+        "input": {
+            "action": "create_voice",
+            "target_model": "cosyvoice-v3-flash",
+            "prefix": prefix,
+            "url": voice_url,
+            "language_hints": ["zh"]
+        }
+    }
+    response = requests.post(os.getenv('VOICE_URL'), headers=headers, json=data)
+    return response.json()
